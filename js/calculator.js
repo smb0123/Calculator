399f907cd5d8627e1230805398265403fb7eab6c
devwqc <devwqc@gmail.com>
2024-01-30 00:01:04 +0900

    feat: 키보드로 받을 수 있는 입력 값들 선언

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,52 @@
 const btn = document.querySelector('.button').children;
 
-const changeColor = function(e) {
+const changeColor = function (e) {
   e.target.style.backgroundColor = 'gray';
-}
+};
 
-const originColor = function(e) {
+const originColor = function (e) {
   e.target.style.backgroundColor = 'white';
-}
+};
 
-for(let i of btn) {
+for (let i of btn) {
   i.addEventListener('mousedown', changeColor);
   i.addEventListener('mouseup', originColor);
 }
 
+const display = document.querySelector('.text');
+const buttonWrapper = document.querySelector('.button');
+
+const keys = [
+  13, 27, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 88, 189, 190, 191,
+];
+
+const shiftKeys = [48, 53, 56, 57, 88, 187];
+
+display.textContent = '';
+
+function calculate(e) {
+  console.log(e);
+  const key = e.key;
+  const keyCode = e.keyCode;
+  const shiftKey = e.shiftKey;
+
+  if (
+    (shiftKey && !shiftKeys.includes(keyCode)) ||
+    (!shiftKey && !keys.includes(keyCode))
+  ) {
+    return;
+  }
+
+  if (keyCode === 13) {
+    console.log('TODO: 계산');
+    return;
+  } else if (keyCode === 27) {
+    console.log('TODO: 클리어');
+    display.textContent = '';
+    return;
+  }
+
+  display.textContent += key;
+}
+
+document.addEventListener('keyup', calculate);
