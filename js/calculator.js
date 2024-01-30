b35ff1cc15616483b85ae3fe0f1a67ad7b1b6894
codingaring <sksms2104@naver.com>
2024-01-31 00:04:08 +0900

    [Style] calculator 레이아웃 및 스타일 적용

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,1 @@
-const btn = document.querySelector('.button').children;
-
-const changeColor = function(e) {
-  e.target.style.backgroundColor = 'gray';
-}
-
-const originColor = function(e) {
-  e.target.style.backgroundColor = 'white';
-}
-
-for(let i of btn) {
-  i.addEventListener('mousedown', changeColor);
-  i.addEventListener('mouseup', originColor);
-}
-
+import { querySelector } from "./utils/querySelector.js";
