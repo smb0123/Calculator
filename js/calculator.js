68744191f7e9615ebbee4e09b79d9145f53f3443
HaeJungg <gpwnd0120@gmail.com>
2024-01-30 23:49:22 +0900

    ✨feat : 버튼이나 키패드 누르면 숫자, 연산자 추가

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,5 @@
 const btn = document.querySelector('.button').children;
+const displayText = document.querySelector('#displayText')
 
 const changeColor = function(e) {
   e.target.style.backgroundColor = 'gray';
@@ -12,4 +13,39 @@ for(let i of btn) {
   i.addEventListener('mousedown', changeColor);
   i.addEventListener('mouseup', originColor);
 }
+
+
+function addToDisplay(text) {
+    displayText.textContent += text;
+}
+
+function btnClick(button, text) {
+  document.querySelector(button).addEventListener('click', function() {
+    const currentLength = displayText.textContent.length;
+    if(currentLength >= 14) {
+      alert('이 이상 입력할 수 없습니다!')
+    } else {
+      addToDisplay(text)
+    }
+  });
+}
+
+btnClick('.zero', 0);
+btnClick('.one', 1);
+btnClick('.two', 2);
+btnClick('.three', 3);
+btnClick('.four', 4);
+btnClick('.five', 5);
+btnClick('.six', 6);
+btnClick('.seven', 7);
+btnClick('.eight', 8);
+btnClick('.nine', 9);
+btnClick('.left-bracket', '(');
+btnClick('.right-bracket', ')');
+btnClick('.remain', '%');
+btnClick('.divide', '/');
+btnClick('.multiply', 'x');
+btnClick('.subtract', '-');
+btnClick('.plus', '+');
+btnClick('.point', '.');
 
