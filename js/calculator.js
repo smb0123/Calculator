fc1dde33fcf16d29802c2e99ec6c055589d43c09
devwqc <devwqc@gmail.com>
2024-02-01 13:58:53 +0900

    feat: Add dark-mode

--- js/calculator.js
+++ js/calculator.js
@@ -1,22 +1,13 @@
-import { keyObj } from './constants.js';
-
-const btn = document.querySelector('.button').children;
-
-const changeColor = function (e) {
-  e.target.style.backgroundColor = 'gray';
-};
-
-const originColor = function (e) {
-  e.target.style.backgroundColor = 'white';
-};
-
-for (let i of btn) {
-  i.addEventListener('mousedown', changeColor);
-  i.addEventListener('mouseup', originColor);
-}
+import {
+  KEY_OBJ,
+  DARK_MODE_TYPE,
+  DARK_MODE_IMG_SRC,
+  DARK_MODE_IMG_ALT,
+} from './constants.js';
 
 const display = document.querySelector('.text');
 const buttonWrapper = document.querySelector('.button');
+const darkModeToggler = document.querySelector('.darkModeToggler');
 
 function calculate() {
   try {
@@ -29,20 +20,20 @@ function calculate() {
 }
 
 function processKey(key) {
-  if (!keyObj.hasOwnProperty(key)) return;
+  if (!KEY_OBJ.hasOwnProperty(key)) return;
 
-  switch (keyObj[key]) {
-    case keyObj.backspace:
+  switch (KEY_OBJ[key]) {
+    case KEY_OBJ.backspace:
       backSpaceDisplay();
       break;
-    case keyObj.escape:
+    case KEY_OBJ.escape:
       clearDisplay();
       break;
-    case keyObj.enter:
+    case KEY_OBJ.enter:
       calculate();
       break;
     default:
-      display.textContent += keyObj[key];
+      display.textContent += KEY_OBJ[key];
       break;
   }
 }
@@ -60,28 +51,52 @@ function handleKeydown(e) {
   const scope = e.currentTarget ? e.currentTarget : document;
   const key = e.key?.toLowerCase();
 
-  if (!keyObj.hasOwnProperty(key)) return;
+  if (!KEY_OBJ.hasOwnProperty(key)) return;
 
-  const element = scope.querySelector(`button[data-key="${keyObj[key]}"]`);
+  const element = scope.querySelector(`button[data-key="${KEY_OBJ[key]}"]`);
 
   if (element) {
-    element.style.backgroundColor = 'gray';
+    element.classList.add('active');
     setTimeout(() => {
-      element.style.backgroundColor = 'white';
+      element.classList.remove('active');
     }, 100);
   }
 
   processKey(key);
 }
 
-function handleClick(e) {
+function handleClickKey(e) {
   const target = e.target;
   const key = target.dataset.key?.toLowerCase();
 
-  if (!keyObj.hasOwnProperty(key)) return;
+  if (!KEY_OBJ.hasOwnProperty(key)) return;
 
   processKey(key);
 }
 
+function handleClickMode(e) {
+  e.stopPropagation();
+
+  const target = e.target;
+
+  if (!target.classList.contains('darkModeToggler')) return;
+
+  toggleMode();
+}
+
+function toggleMode() {
+  const calculatorBox = document.querySelector('.calculator-box');
+
+  const mode = darkModeToggler.dataset.mode?.toLowerCase();
+  const changeMode =
+    mode === DARK_MODE_TYPE.DARK ? DARK_MODE_TYPE.LIGHT : DARK_MODE_TYPE.DARK;
+  calculatorBox.classList.remove(mode);
+  calculatorBox.classList.add(changeMode);
+  darkModeToggler.dataset.mode = changeMode;
+  darkModeToggler.setAttribute('src', DARK_MODE_IMG_SRC[changeMode]);
+  darkModeToggler.setAttribute('alt', DARK_MODE_IMG_ALT[changeMode]);
+}
+
 document.addEventListener('keydown', handleKeydown);
-buttonWrapper.addEventListener('click', handleClick);
+buttonWrapper.addEventListener('click', handleClickKey);
+darkModeToggler.addEventListener('click', handleClickMode);
--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
-export const keyObj = {
+export const KEY_OBJ = {
   0: '0',
   1: '1',
   2: '2',
@@ -22,4 +22,19 @@ export const keyObj = {
   '=': 'enter',
   escape: 'escape',
   backspace: 'backspace',
+};
+
+export const DARK_MODE_TYPE = {
+  LIGHT: 'light',
+  DARK: 'dark',
+};
+
+export const DARK_MODE_IMG_SRC = {
+  [DARK_MODE_TYPE.LIGHT]: '/images/sun.png',
+  [DARK_MODE_TYPE.DARK]: '/images/moon.png',
+};
+
+export const DARK_MODE_IMG_ALT = {
+  [DARK_MODE_TYPE.LIGHT]: '라이트모드 해',
+  [DARK_MODE_TYPE.DARK]: '다크모드 달',
 };
